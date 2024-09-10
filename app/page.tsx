c324cbc6f13a397f47006aeaf8a3991cb8504e28
Priyansu Singh <priyansusingh3344@gmail.com>
2024-09-10 11:26:42 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   const router = useRouter();
   return <div>
-         got to signup <button onClick={async () => {
+        Hello User got to signup <button onClick={async () => {
                    router.push("user/signup")
 }} type="button" className="mt-8 w-30 text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign up</button>
 
